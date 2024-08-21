d48b10638c940547e8675b5d1407136c85f1344b
banyar-shin <banyar.minshin@gmail.com>
2024-08-21 16:04:57 -0700

    install fzf and z on linux

--- .config/zsh/linux.zsh
+++ .config/zsh/linux.zsh
@@ -120,3 +120,11 @@ source ~/powerlevel10k/powerlevel10k.zsh-theme
 
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source <(fzf --zsh)
+
+eval "$(zoxide init zsh)"
+
