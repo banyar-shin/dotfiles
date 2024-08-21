88a1f7bc8d8137f7c13ef7b40e87917b07e920f6
Banyar Shin <banyar.minshin@gmail.com>
2024-08-20 21:05:00 -0700

    Update linux.zsh

--- .config/zsh/linux.zsh
+++ .config/zsh/linux.zsh
@@ -115,3 +115,5 @@ source ~/powerlevel10k/powerlevel10k.zsh-theme
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+alias config='/usr/bin/git --git-dir=/Users/banyar/.cfg/ --work-tree=/Users/banyar'
