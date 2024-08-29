1839685216b40bd82c04df0841fcd72dca44df51
Banyar Shin <banyar.minshin@gmail.com>
2024-08-28 17:43:23 -0700

    merge pr: "hypr: change keybinds (#5)"

--- .config/kitty/generate_kitty_conf.sh
+++ .config/kitty/generate_kitty_conf.sh
@@ -3,8 +3,8 @@
 # Ensure the environment variable is set
 if command -v pacman >/dev/null; then
   echo "kitty.conf generator: Linux detected!"
-  export KITTY_FONT_SIZE=12.0
-  echo "Font size set to 12.0"
+  export KITTY_FONT_SIZE=14.0
+  echo "Font size set to 14.0"
 
 elif [[ $(uname) == "Darwin" ]]; then
   echo "kitty.conf generator: MacOS detected!"
