eca843788cd6fb16bfe47e3fb6fb4070afce068e
banyar-shin <banyar.minshin@gmail.com>
2024-12-12 13:31:10 -0800

    fix clipboard + add wezwin script

---
+++ .config/fish/functions/wezwin.fish
@@ -0,0 +1,4 @@
+function wezwin --wraps=nvim\ /mnt/Users/banya/.wezterm.lua\n --wraps='nvim /mnt/c/Users/banya/.wezterm.lua' --description 'alias wezwin=nvim /mnt/c/Users/banya/.wezterm.lua'
+  nvim /mnt/c/Users/banya/.wezterm.lua $argv
+        
+end
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local os = jit.os
+
+if os ~= "OSX" then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
