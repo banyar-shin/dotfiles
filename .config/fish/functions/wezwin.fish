8ecbd71fdf1ede67653d9eba281e6820b492d3b8
Banyar Shin <banyar.minshin@gmail.com>
2025-01-13 12:57:45 -0800

    Delete .config/fish/functions/wezwin.fish

--- .config/fish/functions/wezwin.fish
+++
@@ -1,4 +0,0 @@
-function wezwin --wraps=nvim\ /mnt/Users/banya/.wezterm.lua\n --wraps='nvim /mnt/c/Users/banya/.wezterm.lua' --description 'alias wezwin=nvim /mnt/c/Users/banya/.wezterm.lua'
-  nvim /mnt/c/Users/banya/.wezterm.lua $argv
-        
-end
