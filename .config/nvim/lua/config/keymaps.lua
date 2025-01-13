c447de8b04c0eaa383ab19e4738cefa7921b52f3
banyar-shin <banyar.minshin@gmail.com>
2025-01-13 13:15:52 -0800

    update nvim plugins

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.keymap.set("n", "<CR>", '@="m`o<C-V><Esc>``"<CR>')
-vim.keymap.set("n", "<S-CR>", '@="m`O<C-V><Esc>``"<CR>')
