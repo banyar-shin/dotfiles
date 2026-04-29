703b1da267193f9718a18b1a0a95b300da20031e
banyar-shin <banyar.minshin@gmail.com>
2026-04-29 14:52:02 -0700

    speed up fish startup: lazy-load conda, inline theme, refresh backup

---
+++ .config/fish/conf.d/conda-lazy.fish
@@ -0,0 +1,12 @@
+set -g __conda_bin /Users/banyar-ego/miniconda3/bin/conda
+set -g __conda_hook_cache ~/.cache/fish/conda-hook.fish
+
+function conda --description "Lazy conda wrapper: sources cached hook on first call"
+    if not test -f $__conda_hook_cache
+        mkdir -p (dirname $__conda_hook_cache)
+        $__conda_bin shell.fish hook >$__conda_hook_cache
+    end
+    functions --erase conda
+    source $__conda_hook_cache
+    conda $argv
+end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,18 +5,7 @@
 # https://github.com/fish-shell/fish-shell/blob/master/share/config.fish
 # https://github.com/fish-shell/fish-shell/blob/da32b6c172dcfe54c9dc4f19e46f35680fc8a91a/share/config.fish#L257-L269
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/banyar-ego/miniconda3/bin/conda
-    eval /Users/banyar-ego/miniconda3/bin/conda "shell.fish" hook $argv | source
-else
-    if test -f "/Users/banyar-ego/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/Users/banyar-ego/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH /Users/banyar-ego/miniconda3/bin $PATH
-    end
-end
-# <<< conda initialize <<<
+# conda is lazy-loaded via ~/.config/fish/conf.d/conda-lazy.fish
 
 # pnpm
 set -gx PNPM_HOME /Users/banyar-ego/Library/pnpm
@@ -25,7 +14,7 @@ if not string match -q -- $PNPM_HOME $PATH
 end
 # pnpm end
 
-source ~/.config/fish/functions/env-switch.fish
+# source ~/.config/fish/functions/env-switch.fish  # disabled — slow conda subprocess on every shell
 
 # tabtab source for electron-forge package
 # uninstall by removing these lines or running `tabtab uninstall electron-forge`
@@ -71,3 +60,32 @@ function tmux-immvrse
         tmux new -s immvrse -c ~/git-repos/immvrse
     end
 end
+
+# Syntax highlighting + pager colors (inlined from fish 4.3 frozen-theme migration)
+set --global fish_color_autosuggestion brblack
+set --global fish_color_cancel -r
+set --global fish_color_command blue
+set --global fish_color_comment red
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end green
+set --global fish_color_error brred
+set --global fish_color_escape brcyan
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_normal normal
+set --global fish_color_operator brcyan
+set --global fish_color_param cyan
+set --global fish_color_quote yellow
+set --global fish_color_redirection cyan --bold
+set --global fish_color_search_match white --background=brblack
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description yellow -i
+set --global fish_pager_color_prefix normal --bold --underline
+set --global fish_pager_color_progress brwhite --background=cyan
+set --global fish_pager_color_selected_background -r
